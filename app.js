aa8646152b642437844a61204ce8f398805b835a
Diana Coelho <dianasouza@MacBook-Pro-de-Diana.local>
2019-06-02 20:05:24 -0300

    Commit inicial

---
+++ app.js
@@ -0,0 +1,44 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+
+var campgrounds = [
+  {name: "Rock Camp", image: "https://images.unsplash.com/photo-1455763916899-e8b50eca9967?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"},
+  {name: "Caipirinha Camp", image: "https://images.unsplash.com/photo-1455496231601-e6195da1f841?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1395&q=80"},
+  {name: "Mozitos Camp", image: "https://images.unsplash.com/photo-1557474295-714ea5fc2557?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1375&q=80"},
+  {name: "Rock Camp", image: "https://images.unsplash.com/photo-1455763916899-e8b50eca9967?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"},
+  {name: "Caipirinha Camp", image: "https://images.unsplash.com/photo-1455496231601-e6195da1f841?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1395&q=80"},
+  {name: "Mozitos Camp", image: "https://images.unsplash.com/photo-1557474295-714ea5fc2557?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1375&q=80"},
+  {name: "Rock Camp", image: "https://images.unsplash.com/photo-1455763916899-e8b50eca9967?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"},
+  {name: "Caipirinha Camp", image: "https://images.unsplash.com/photo-1455496231601-e6195da1f841?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1395&q=80"},
+  {name: "Mozitos Camp", image: "https://images.unsplash.com/photo-1557474295-714ea5fc2557?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1375&q=80"}
+];
+
+app.get('/', (req, res) => {
+    res.render("landing");
+  });
+  
+app.get('/campgrounds', (req, res) => {
+  res.render("campgrounds", {campgrounds: campgrounds});
+});
+
+app.post('/campgrounds', (req, res)=> {
+  //pegar os dados do formulario e adicionar no array de campgrounds
+  var name = req.body.name;
+  var image = req.body.image;
+  var newCampground = {name:name, image:image}
+  campgrounds.push(newCampground);
+  //redirecionar para a landing
+  res.redirect("/campgrounds"); 
+});
+
+app.get("/campgrounds/new", (req, res) => {
+  res.render("new.ejs")
+});
+
+  app.listen(3000, () => {
+    console.log('Example app listening on port 3000!');
+  });
